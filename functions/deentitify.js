4ac1e9fccc1a864e7aa33c7855e10af3a3b22ba2
Friedrich Clausen <friedrich.clausen@blackboard.com>
2018-07-23 11:48:18 +1000

    Finish deentitify

--- functions/deentitify.js
+++ functions/deentitify.js
@@ -3,4 +3,22 @@ Function.prototype.method = function(name, func) {
   return this;
 };
 
+String.method('deentityify', function() {
+  var entity = {
+    quot: '"',
+    lt: '<',
+    gt: '>'
+  };
 
+  return function() {
+    return this.replace(/&([^&;]+);/g,
+      function(a, b) {
+        var r = entity[b];
+        return typeof r === 'string' ? r : a;
+      }
+    )
+  };
+}());
+
+var toDo = '&lt;&quot;&gt;';
+console.log(toDo + " -> " + toDo.deentityify());
