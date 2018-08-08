f38a2bd4a390860a511c23359fc0b9fae4c00f5e
Friedrich Clausen <friedrich.clausen@blackboard.com>
2018-08-08 17:22:13 +1000

    Use event handling. NEXT: Figure out string func part

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ### My own notes of the seminal Javascript: The Good Parts
 
 There are many notes like these but these are mine. I am not trying to be exhaustive - just jotting things down to help
-cement the concepts
+cement the concepts.
+
+I also rename variables and other small tweaks to make the code more understandable to me. Don't expect consistent
+formatting; this is a place where I hammer concepts into my brain.
---
+++ inheritance/parts.js
@@ -0,0 +1,76 @@
+// Add simple event handling to the provided object.
+// - Add `on` method
+// - Add `fire` method
+// - Add private event registry
+
+var eventuality = function(that) {
+  var registry = {};
+
+  that.fire = function(event) {
+    // Fire an event on the object to which we are attached. The event is
+    // either a string containing the name of the event or an object containing
+    // a `type` property which has the name of the event. How do we know which
+    // handler should handle a given event? Why, easy! We register a handler
+    // with the `on` method with the same name.
+    var type = typeof event == 'string' ? event : event.type;
+
+    // If an array of handlers (so lots of handlers for a given event) then
+    // loop through it and execute the handlers in order
+    var handler; // Current handler
+    var allHandlers; // An array of handlers
+    var func; // Function associated with a particular handler
+    if (registry.hasOwnProperty(type)) {
+      allHandlers = registry[type];
+      for (var i = 0; i < allHandlers.length; i += 1) {
+        handler = allHandlers[i];
+        // EXPLAIN ???
+        func = handler.method
+        if (typeof func === 'string') {
+          func = this[func];
+        }
+
+        // Invoke the handler and, if params present, then use them otherwise
+        // pass the event object
+        func.apply(this, handler.parameters || [event]);
+        // Or maybe: func(handler.parameters || [event]);
+      }
+    } else {
+      console.log('Did not find handler for ' + type);
+    }
+    return this;
+  };
+
+  that.on = function(type, method, parameters) {
+    // Register an event by making a handler record in the handler array.
+    // Making an array if one does not already exist.
+    var handler = {
+      method: method,
+      parameters: parameters
+    };
+
+    if (registry.hasOwnProperty(type)) {
+      registry[type].push(handler);
+    } else {
+      registry[type] = [handler];
+    }
+
+    return this;
+  };
+  return that;
+};
+
+var freddoObject = {
+  name: 'freddo'
+};
+
+freddoObject = eventuality(freddoObject);
+var myHandler = function(data) {
+  if (data) {
+    console.log('Running handler for freddo! With data ' + data);
+  }
+};
+
+freddoObject.on('myEvent', myHandler, ['some data weee']);
+freddoObject.fire('myEvent');
+freddoObject.on('myEventNoParam', myHandler);
+freddoObject.fire('myEventNoParam');
